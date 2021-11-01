fe202e9a97143ad0a1710d999772c92aaa5b3a62
Colin Mothersead <cmothersead@gmail.com>
2021-10-31 21:15:02 -0400

    Getting roster started

--- frstswimming/swimmers/models.py
+++ frstswimming/swimmers/models.py
@@ -1,12 +1,21 @@
+from datetime import date
 from django.db import models
+from django.utils import timezone
 
 class Swimmer(models.Model):
     first_name = models.CharField(max_length=45)
-    preferred_name = models.CharField(max_length=45, blank=True)
+    preferred_name = models.CharField(max_length=45, null=True)
     middle_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     gender = models.CharField(max_length=1)
     birthday = models.DateField()
 
     def __str__(self) -> str:
-        return self.preferred_name + ' ' + self.last_name
+        return self.first_name + ' ' + self.last_name
+
+    def age_on(self, date):
+        return date.year - self.birthday.year - ((date.month, date.day) < (self.birthday.month, self.birthday.day))
+    
+    @property
+    def age(self):
+        return self.age_on(date.today())
