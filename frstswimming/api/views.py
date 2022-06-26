5c918c1fcca0d1c858ed96f19b2d0781d9bca4a1
Colin Mothersead <cmothersead@gmail.com>
2022-06-26 18:33:33 -0400

    Move static files into separate directory

--- frstswimming/api/views.py
+++ frstswimming/api/views.py
@@ -2,15 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import SeasonSerializer, SwimmerSerializer
 from .models import Season, Swimmer
 
-# class IndexView(generic.ListView):
-#     model = Swimmer
-
-#     def get_queryset(self):
-#         query_set = Swimmer.objects.all().prefetch_related('groups')
-#         return query_set
-
-# class DetailView(generic.DetailView):
-#     model = Swimmer
 
 class SwimmerViewSet(ModelViewSet):
     queryset = Swimmer.objects.all().prefetch_related('groups')
