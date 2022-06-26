d49762c6ddb936797415e9232633d9a1483c380c
Colin Mothersead <cmothersead@gmail.com>
2022-06-26 15:17:54 -0400

    Not sure if these did anything, but I added them

--- roster/src/lib/SwimmerDetail.svelte
+++ roster/src/lib/SwimmerDetail.svelte
@@ -1,33 +1,37 @@
 <script>
     export let first_name, pref_name="", middle_name, last_name, sex, birthday;
+
+    function alertTest(){
+        alert("The button was pressed");
+    }
 </script>
 
 <h1>Swimmer Data</h1>
 <form>
-    <div class="row">
-        <div class="col">
+    <div class="row form-row">
+        <div class="col form-group">
             <label for="first_name">First</label>
             <input type="text" id="first_name" bind:value={first_name} class="form-control" placeholder="First">
         </div>
-        <div class="col">
+        <div class="col form-group">
             <label for="pref_name">Preferred Name</label>
             <input type="text" id="pref_name" bind:value={pref_name} class="form-control" placeholder="Preferred">
         </div>
-        <div class="col">
+        <div class="col form-group">
             <label for="middle_name">Middle Name</label>
             <input type="text" id="middle_name" bind:value={middle_name} class="form-control" placeholder="Middle">
         </div>
-        <div class="col">
+        <div class="col form-group">
             <label for="last_name">Last</label>
             <input type="text" id="last_name" bind:value={last_name} class="form-control" placeholder="Last">
         </div>
     </div>
-    <div class="row">
-        <div class="col">
+    <div class="row form-row">
+        <div class="col form-group">
             <label for="birthday">Birthday</label>
             <input type="date" id="birthday" bind:value={birthday} class="form-control">
         </div>
-        <div class="col">
+        <div class="col form-group">
             <label for="sex">Sex</label>
             <select id="sex" bind:value={sex} class="form-control">
                 <option value="M">Male</option>
@@ -35,6 +39,8 @@
             </select>
         </div>
     </div>
+    <button type="button" class="btn btn-outline-danger" on:click="{alertTest}">Cancel</button>
+    <button type="button" class="btn btn-success">Save</button>
 </form>
 
 <!-- Latest compiled and minified CSS -->
